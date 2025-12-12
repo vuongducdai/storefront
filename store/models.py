85029ce3ae8ddba00723533d6afc7dc4627b2e1a
daivd <daivuong7696@gmail.com>
2025-12-12 22:29:40 +0700

    Resolve circular dependency

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Promotion(models.Model):
     title = models.CharField(max_length=255)
