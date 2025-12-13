d05390ebd3aa68be724c9246133d5a90dbc4b49a
daivd <daivuong7696@gmail.com>
2025-12-13 09:45:11 +0700

    feat: Add a `slug` field to the `Product` model with a new migration.

--- store/models.py
+++ store/models.py
@@ -10,6 +10,7 @@ class Promotion(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(default='-')
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
